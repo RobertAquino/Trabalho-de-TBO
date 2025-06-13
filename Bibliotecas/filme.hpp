9c3d7cadc9f869ff342cc12ac8ec4a236e0fc8d2
RobertAquino <orobertaquino@gmail.com>
2025-06-13 16:48:30 -0300

    Algumas alterações

--- Bibliotecas/filme.hpp
+++ Bibliotecas/filme.hpp
@@ -3,6 +3,19 @@
 #include "cinema.hpp"
 #include <iostream>
 
+enum class Genres
+{
+    Action = 0, Adventure = 1, Animation = 2,
+    Biography = 3, Comedy = 4, Crime = 5,
+    Documentary = 6, Drama = 7, Family = 8,
+    Fantansy = 9, Game_Show = 10, History = 11,
+    Horror = 12, Music = 13, Music = 14,
+    Musicial = 15, Mystery = 16, News = 17,
+    Reality_Tv = 18, Romance = 19, Sci_fi = 20,
+    Short = 21, Sport = 22, Talk_Show = 23,
+    Thriller = 24, War = 25, Adult = 26
+};
+
 class Filme{
 public:
 
@@ -15,10 +28,21 @@ public:
     short startYear;
     short endYear;
     int runTimesMinutes;
-    std::string genres[];
+    std::string genre;
 
     Filme();
     ~Filme();
 
-
+    std::string getTitleType()
+    {
+        return titleType;
+    }
+    Cinema getCinema()
+    {
+        return cinema;
+    }
+    std::string getGenres()
+    {
+        return genre;
+    }
 };
--- Bibliotecas/genresFilter.hpp
+++ Bibliotecas/genresFilter.hpp
@@ -3,21 +3,17 @@
 #include "filme.hpp"
 #include "arrayList.hpp"
 
-enum class genres
-{
-    Action = 0, Adventure = 1, Animation = 2,
-    Biography = 3, Comedy = 4, Crime = 5,
-    Documentary = 6, Drama = 7, Family = 8,
-    Fantansy = 9, Game_Show = 10, History = 11,
-    Horror = 12, Music = 13, Music = 14,
-    Musicial = 15, Mystery = 16, News = 17,
-    Reality_Tv = 18, Romance = 19, Sci_fi = 20,
-    Short = 21, Sport = 22, Talk_Show = 23,
-    Thriller = 24, War = 25, Adult = 26
-};
-
 class genresFilter
 {
     public:
 
-};
+    ArrayList<Filme> genresFilter;
+
+    ArrayList<Filme> getGenresFilter(ArrayList<Filme> filmes, ArrayList<Filme> &genresFilter);
+
+};  
+
+    ArrayList<Filme> getGenresFilter(ArrayList<Filme> filmes, ArrayList<Filme> &genresFilter, Genres genre)
+    {
+        ArrayList<Filme> filme;   
+    }
